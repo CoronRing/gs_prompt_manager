7125fa1e5266a1cee683f2c3f9193969dc25b9cd
Guan Zheng Huang <guanzheng.huang@hotmail.com>
2026-02-19 13:13:17 -0800

    INITIAL COMMIT

---
+++ test/sample_prompts/prompt_hello_world.py
@@ -0,0 +1,16 @@
+from gs_prompt_manager import PromptBase, PromptManager
+
+class PromptHelloWorld(PromptBase):
+    """
+    A simple prompt that returns "Hello, World!".
+    """
+
+    def set_prompt_chat(self) -> str:
+        """
+        Generate the prompt string.
+
+        Returns:
+            str: The prompt string "Hello, World!".
+        """
+        return "Hello, World!"
+    
